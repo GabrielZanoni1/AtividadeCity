aca41bc198a3ccfb8187b392fa7b9d9a1cc98265
gabriel <gabrielzf06@gmail.com>
2025-05-17 00:25:35 -0300

    Adicionando a injeção de dependência

--- AtividadeCity/Program.cs
+++ AtividadeCity/Program.cs
@@ -1,8 +1,11 @@
+using AtividadeCity.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<UsuarioRepositorio>();
+builder.Services.AddScoped<ProdutoRepositorio>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
