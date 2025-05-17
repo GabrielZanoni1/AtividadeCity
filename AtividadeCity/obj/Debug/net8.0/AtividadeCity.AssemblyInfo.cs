b31c7d6124c7b3b4b99edc2371f72c8085d05153
gabriel <gabrielzf06@gmail.com>
2025-05-17 01:01:12 -0300

    Ajustando as Views

--- AtividadeCity/obj/Debug/net8.0/AtividadeCity.AssemblyInfo.cs
+++ AtividadeCity/obj/Debug/net8.0/AtividadeCity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadeCity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1fc7058a24b6defd291c78e3272affe7055e23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8d0499dfdb860cec090ffe18047187c9f5994e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadeCity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadeCity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
