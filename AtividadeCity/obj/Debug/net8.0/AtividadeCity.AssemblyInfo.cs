b826db856e3e9bf40007f314550a675cd3a7379a
gabriel <gabrielzf06@gmail.com>
2025-05-16 21:57:59 -0300

    Adicionando as Models

--- AtividadeCity/obj/Debug/net8.0/AtividadeCity.AssemblyInfo.cs
+++ AtividadeCity/obj/Debug/net8.0/AtividadeCity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadeCity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea250094c2f8fdf158a5e289857dac8c19049fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6a38c291e8e565cfc7e5e8cb692bcf658ed603")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadeCity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadeCity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
